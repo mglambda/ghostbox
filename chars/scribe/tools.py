56a7fd23b74d02d8de156ea23bdb640043009142
Marius Gerdes <integr@gmail.com>
2024-05-01 15:00:24 +0200

    Fix command-r template.

--- chars/scribe/tools.py
+++ chars/scribe/tools.py
@@ -1,8 +1,13 @@
+import os
 
 
 def take_note(text):
     """Take down a note which will be written to a file on the hard disk."""
-    f = open("/hoem/marius/ainote.txt", "a")
-    f.write(note)
+    file = os.path.expanduser("~/ai_scribe_note.txt")
+    if os.path.isfile(file):
+        f = open(file, "a")
+    else:
+        f = open(file, "w")
+    f.write(text + "\n")
     f.close()
-    
+    return ""
