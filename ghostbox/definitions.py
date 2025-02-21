052b6754c15d4a251f311158d66e69c6f5212324
Marius Gerdes <integr@gmail.com>
2025-02-21 04:12:42 +0100

    Add definitions for template special values.

--- ghostbox/definitions.py
+++ ghostbox/definitions.py
@@ -3,6 +3,9 @@ from enum import Enum
 
 LLMBackend = Enum("LLMBackend", "generic legacy llamacpp koboldcpp openai dummy")
 
+# this isn't used yet anywhere, but it's nice to have here already for documentation
+PromptFormatTemplateSpecialValue = Enum("PromptFormatTemplateSpecialValue", "auto guess raw")
+
 api_default_options = {
     "color" : False,
     "verbose" : False,
