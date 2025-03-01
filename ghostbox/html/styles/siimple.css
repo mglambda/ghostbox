5769e20d64c0cee101c5cfd51540912cf2ac87e8
Marius Gerdes <integr@gmail.com>
2025-03-01 05:45:24 +0100

    Add stylesheet and style selector. Thanks to the excellent drop-in minimal css https://dohliam.github.io/dropin-minimal-css/

---
+++ ghostbox/html/styles/siimple.css
@@ -0,0 +1,6 @@
+// Export the same API from @siimple/core
+export {css, mergeConfig, mergeStyles} from "@siimple/core";
+export {injectModules} from "@siimple/modules";
+
+// Export base theme
+export {default as base} from "@siimple/preset-base";
