8ea1fbd37776164934b9c6f7df31e209b2ba1524
Marius Gerdes <integr@gmail.com>
2026-02-14 07:53:41 +0100

    Add typing to output_formatter.py

--- ghostbox/output_formatter.py
+++ ghostbox/output_formatter.py
@@ -1,8 +1,9 @@
 from abc import ABC, abstractmethod
 import re
-from functools import *
-from ghostbox.util import *
+from functools import reduce
+from ghostbox.util import stringToColor, stringToStyle, wrapColorStyle
 from colorama import Fore, Style
+from typing import List, Callable, Any
 
 class OutputFormatter(ABC):
     """An interface for formatting the chat history. The OutputFormatter interface is used both for formatted text that is presented to the user, as well as formatting text to send to the backend, e.g. to prepend 'Bob:' style chat prompts.
@@ -16,26 +17,28 @@ f.format(w) == f.format(f.format(w))
 for all strings w. In other words, f is idempotent under repeated application. We might get a string 'How are you, Sally?' which the formatter turns into 'Bob: How are you, Sally?', but we don't want this to turn into 'Bob: Bob: How are you, Sally?' in case the LLM starts to prepend 'Bob: ' itself."""
 
     @abstractmethod
-    def format(self, w):
+    def format(self, w: str) -> str:
         pass
 
-    def compose(xs):
+    @staticmethod
+    def compose(xs: List["OutputFormatter"]) -> "ComposedFormatter":
         """Takes a list of objects supporting the OutputFormatter interface and returns a ComposedFormatter that is the result of their composition."""
-        return reduce(ComposedFormatter, xs, IdentityFormatter())
+        return reduce(ComposedFormatter, xs, IdentityFormatter()) # type: ignore
 
-    def __add__(self, other):
+    def __add__(self, other: "OutputFormatter") -> "ComposedFormatter":
         return ComposedFormatter(self, other)
     
-    def sequence(xs, w):
+    @staticmethod
+    def sequence(xs: List["OutputFormatter"], w: str) -> str:
         """Takes a list of OutputFormatters xs and applies them to w in sequence. Tip: Read the list from left to right and imagine applying each formatter to the string in succession."""
         return reduce(lambda v, f: f.format(v), xs, w)
     
 class IdentityFormatter(OutputFormatter):
     """This formatter returns its input unchanged."""
-    def __init__(self):
+    def __init__(self) -> None:
         pass
     
-    def format(self, w):
+    def format(self, w: str) -> str:
         return w
 
 class ComposedFormatter(OutputFormatter):
@@ -63,34 +66,34 @@ c.format(w) == a.format(w)
 
     at least not for all a and b. So in general, composition of formatters does not commute."""
 
-    def __init__(self, a, b):
+    def __init__(self, a: OutputFormatter, b: OutputFormatter) -> None:
         self.a = a
         self.b = b
 
-    def format(self, w):
+    def format(self, w: str) -> str:
         return self.a.format(self.b.format(w))
     
 class NicknameFormatter(OutputFormatter):
     """This formatter prepends chat names, according to a given decorator. By default, names are decorated with colon, as in 'Bob: '."""
     
-    def __init__(self, nickname, decorator_func=lambda w: w + ": "):
-        self.decorator_func=decorator_func
+    def __init__(self, nickname: str, decorator_func: Callable[[str], str] = lambda w: w + ": ") -> None:
+        self.decorator_func = decorator_func
         self.nickname = nickname
 
-    def setNickname(self, nickname):
+    def setNickname(self, nickname: str) -> "NicknameFormatter":
         self.nickname = nickname
         return self
         
-    def format(self, w):
-        def f(v, line, nick=self.decorator_func(self.nickname)):
+    def format(self, w: str) -> str:
+        def f(v: str, line: str, nick: str = self.decorator_func(self.nickname)) -> str:
             if line.startswith(nick):
                 return v + "\n" + line
             else:
                 return v + "\n" + nick + line
         return reduce(f, w.split("\n"), "")[1:]
 
-    def unformat(self, w):
-        def f(v, line, nick=self.decorator_func(self.nickname)):
+    def unformat(self, w: str) -> str:
+        def f(v: str, line: str, nick: str = self.decorator_func(self.nickname)) -> str:
             if line.startswith(nick):
                 return v + "\n" + line[len(nick):]
             else:
@@ -99,19 +102,19 @@ class NicknameFormatter(OutputFormatter):
 
 class NicknameRemover(OutputFormatter):
     """Removes prepended nicknames according to a supplied decorator. By default, this removes stuff like 'Bob: ', and it matches it a little fuzzy to account for spaces."""
-    def __init__(self, nickname, decorator_func=lambda w: w + ": "):
+    def __init__(self, nickname: str, decorator_func: Callable[[str], str] = lambda w: w + ": ") -> None:
         self._f = NicknameFormatter(nickname, decorator_func)
         
-    def format(self, w):
+    def format(self, w: str) -> str:
         return self._f.unformat(w)
     
 class IncompleteSentenceCleaner(OutputFormatter):
     """Removes incomplete sentences at the end of text."""
 
-    def __init__(self,     stopchars = '! . ? ; `'.split(" ")):
+    def __init__(self, stopchars: List[str] = '! . ? ; `'.split(" ")) -> None:
         self.stopchars = stopchars
         
-    def format(self, w):
+    def format(self, w: str) -> str:
         if w == "":
             return w
 
@@ -122,53 +125,57 @@ class IncompleteSentenceCleaner(OutputFormatter):
 
         skip = False
         stopchars = self.stopchars
-        for i in range(len(w)-1, -1, -1):
+        i = len(w) - 1
+        while i >= 0:
             for match in exclusions:
                 if i > match.start() and i < match.end():
                     skip = True
+                    break
 
             if skip:
                 skip = False
+                i -= 1
                 continue
             if w[i] in stopchars:
                 break
+            i -= 1
 
-        if i == 0:
+        if i < 0:
             return ""
         return w[:i+1]
 
 class WhitespaceFormatter(OutputFormatter):
     """Removes whitespace at the beginning and end of text."""
 
-    def format(self, w):
+    def format(self, w: str) -> str:
         return w.strip()
     
 class LonelyPromptCleaner(OutputFormatter):
     """Removes lonely occurrences of trailing chat prompts. Like trailing 'Bob: ' etc, since these are hard to spot with incomplete sentence cleaning. You can supply another decorator besides the default '<nick>: ' pattern. Also note that cleaning is a little fuzzy when it comes to spaces."""
-    def __init__(self, nickname, decorator_func=lambda w: w + ": "):
+    def __init__(self, nickname: str, decorator_func: Callable[[str], str] = lambda w: w + ": ") -> None:
         self.decorator_func = decorator_func
         self.nickname = nickname
 
-    def setNickname(self, nickname):
+    def setNickname(self, nickname: str) -> "LonelyPromptCleaner":
         self.nickname = nickname
         return self
         
-    def format(self, w):
+    def format(self, w: str) -> str:
         prompt = self.decorator_func(self.nickname)
         ws = w.split("\n")
-        return "\n".join(filter(lambda v: not(v in prompt), ws))
+        return "\n".join(filter(lambda v: not(v.strip() == prompt.strip()), ws))
 
 
 class ChatFormatter(OutputFormatter):
     """General purpose chat formatter with some extra functionality. Will prepend nicknames according to a decorator, and cleans up some bogus responses."""
-    def __init__(self, nickname, decorator_func=lambda w: w + ": "):
+    def __init__(self, nickname: str, decorator_func: Callable[[str], str] = lambda w: w + ": ") -> None:
         self.decorator_func = decorator_func
         self.nickname = nickname
 
-    def setNickname(self, nickname):
+    def setNickname(self, nickname: str) -> None:
         self.nickname = nickname
 
-    def format(self, w):
+    def format(self, w: str) -> str:
         return OutputFormatter.sequence([IncompleteSentenceCleaner(),
                                          LonelyPromptCleaner(self.nickname, decorator_func=self.decorator_func),
                                          NicknameFormatter(self.nickname, decorator_func=self.decorator_func)],
@@ -177,12 +184,12 @@ class ChatFormatter(OutputFormatter):
 
 class ColorFormatter(OutputFormatter):
     """Adds a given color and style to the output. Color and style can be provided as strings, e.g. 'red' and 'bright'm which will be converted to ANSI terminal codes."""
-    def __init__(self, color, style="default"):
+    def __init__(self, color: str, style: str = "default") -> None:
         super().__init__()
         self.color = stringToColor(color)
         self.style = stringToStyle(style)
 
-    def format(self, w):
+    def format(self, w: str) -> str:
         return wrapColorStyle(w, self.color, self.style)
 
 class StripThinkingFormatter(OutputFormatter):
@@ -192,10 +199,8 @@ class StripThinkingFormatter(OutputFormatter):
         pattern = r'<think>.*</think>\s*'
         return re.sub(pattern, '', w, flags=re.DOTALL)
     
-DoNothing = IdentityFormatter() # more descriptive name                
-DefaultResponseCleaner = OutputFormatter.compose([IncompleteSentenceCleaner()])
-CleanResponse = DefaultResponseCleaner
+DoNothing: OutputFormatter = IdentityFormatter() # more descriptive name                
+DefaultResponseCleaner: OutputFormatter = OutputFormatter.compose([IncompleteSentenceCleaner()])
+CleanResponse: OutputFormatter = DefaultResponseCleaner
 DefaultResponseCleaner.__doc__ =     """Does minimum cleanup. Intended for AI responses."""
-StripThinking = StripThinkingFormatter()
-
-    
+StripThinking: OutputFormatter = StripThinkingFormatter()
